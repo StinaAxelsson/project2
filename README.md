0d332174f21ab781466c1f60830a64995f990ac2
StinaAxelsson <stina91@live.se>
2021-08-05 12:58:29 +0000

    Trying to make a collision function with no result

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # project2
+
+    let top = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+    if (e.key == "ArrowUp" && top > 0) {
+        player.style.top = top - 20 + "px";
+      }
+      else if (e.key == "ArrowDown" && top <= 400) {
+        player.style.top = top + 20 + "px";
+      }
--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,9 @@
 const player = document.getElementById('player');
 const board = document.getElementById('gameboard');
 
-let cometInterval;
+
 let score = 0;
+
 
 
 // Touchevent and function for playing on a touch screen, for moving the player.
@@ -17,7 +18,6 @@ function handleTouchEvent(e) {
     if (e.touches.length === 0 ) return;
     let touch = e.touches[0];
     player.style.left = (touch.pageX - player.width / 2) + 'px';
-    player.style.top = (touch.pageY - player.width / 2) + 'px';
 }
 
 // Add an Keyevent for when play on desktop, using the up, down, left and right arrown in the gameboard for moving the player.
@@ -30,13 +30,7 @@ window.addEventListener("keydown", (e) => {
     else if (e.key == "ArrowRight" && left <= 350) {
       player.style.left = left + 20 + "px";
     }
-    let top = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
-    if (e.key == "ArrowUp" && top > 0) {
-        player.style.top = top - 20 + "px";
-      }
-      else if (e.key == "ArrowDown" && top <= 400) {
-        player.style.top = top + 20 + "px";
-      }
+
 });
 // This function create an interval of new divs with new comets that randomly will appaere on the gameboard.
 const createObstacle = setInterval(() =>{
@@ -45,6 +39,9 @@ const createObstacle = setInterval(() =>{
   comet.style.left = Math.floor(Math.random() * 350) + 'px';
 
   gameboard.appendChild(comet);
+
+  
+
 }, 2000);
 
 //This function make the new created divs/comets to move from top to bottom.
@@ -55,23 +52,23 @@ const MoveObstacle = setInterval(() => {
       let comet = comets[i];
       let cometTop = parseInt(window.getComputedStyle(comet).getPropertyValue('top'));
 
-      comet.style.top = cometTop + 25 + 'px';
+      comet.style.top = cometTop + 25 + 'px'
+      
     }
+    
   }
+  
 }, 400);
 
 function checkCollision(){
 
 }
 
+
 function setScore(){
 
 }
 
-
-function startGame(){
-
-}
 
 function GameOver(){
 
