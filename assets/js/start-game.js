19fa03982533b79ff46a42f8baaaa4a37ee6ccdc
StinaAxelsson <stina91@live.se>
2021-08-11 23:08:14 +0000

    Add comments in start-game.js

--- assets/js/start-game.js
+++ assets/js/start-game.js
@@ -1,3 +1,9 @@
+
+/**
+ *This function goes with the onclick in the start-game html file. And set the millenium falcon as default image
+ and togheter with the function setPlayerImage in the script.js to set the image decpite on what the
+ user choose in the start-game page.
+ */
 function startGame(imgUrl){
 localStorage.setItem('playerImageUrl', imgUrl);
 window.location.href='gameboard.html';
