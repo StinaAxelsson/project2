f13510952cf3497dc05c720bc273499d1a693545
StinaAxelsson <stina91@live.se>
2021-08-11 23:05:03 +0000

    Add comments in sidebar-menu.js

--- assets/js/sidebar-menu.js
+++ assets/js/sidebar-menu.js
@@ -1,8 +1,15 @@
+
+/**
+ * This function make the sidebar menu opens when clicked on the hamurger icon
+ */
 function openNav() {
     document.getElementById("menu-sidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
   }
   
+  /**
+ * This function make the sidebar menu close when clicked on the X in the menu
+ */
   function closeNav() {
     document.getElementById("menu-sidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
